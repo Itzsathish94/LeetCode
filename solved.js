7baf958aa3d0e41cf70dd2f71d9c208f0b9f74a9
Sathish <rhbsk1994@gmail.com>
2024-12-10 10:23:06 +0530

    Solved 4 array problems 10/12/24

---
+++ solved.js
@@ -0,0 +1,6 @@
+// ARRAYS
+
+// 1) Two sum (no:1) - Array / Easy / Optimized than before / Could optmize / 10/12/24. - PASS
+// 2) Best time to buy and sell stock (no:121) - Array / Easy / optimized / check / 10/12/24.- FAIL
+// 3) Remove Duplicates from Sorted Array (no:26) - Array / Easy / optimized / check / 10/12/24. - FAIL
+// 4) Rotate Array (no:189) - Array / Medium / 10/12/24 - FAIL.
