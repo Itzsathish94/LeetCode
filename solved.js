3eb3544e4492cf825259c9589b95bad804744ee5
Sathish <rhbsk1994@gmail.com>
2024-12-11 09:21:18 +0530

    11/12/24 Solved 3 Array and 1 string problem

--- solved.js
+++ solved.js
@@ -1,6 +1,10 @@
 // ARRAYS
 
-// 1) Two sum (no:1) - Array / Easy / Optimized than before / Could optmize / 10/12/24. - PASS
-// 2) Best time to buy and sell stock (no:121) - Array / Easy / optimized / check / 10/12/24.- FAIL
-// 3) Remove Duplicates from Sorted Array (no:26) - Array / Easy / optimized / check / 10/12/24. - FAIL
-// 4) Rotate Array (no:189) - Array / Medium / 10/12/24 - FAIL.
+// 1) Two sum (no:1) - Array / Easy / Optimized than before / Could optmize / 10/12/24. - P
+// 2) Best time to buy and sell stock (no:121) - Array / Easy / optimized / check / 10/12/24.- RV
+// 3) Remove Duplicates from Sorted Array (no:26) - Array / Easy / optimized / check / 10/12/24. - RV
+// 4) Rotate Array (no:189) - Array / Medium / 10/12/24 - RV
+// 5) Find All Numbers Disappeared in an Array (no:448) - Array / Easy / 11/12/24 - RV.
+// 6) Move Zeroes (no:283) - Array / Easy / 11/12/24 - RV
+// 7) Intersection of Two Arrays II (no:350) - Array / Easy / 11/12/24 - RV
+// 8) Valid Anagram (no.242) - String / Easy / P
